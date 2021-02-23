5fc92469c715111f7d6ef2c5223d4b7a6783cf26
Anderson de Oliveira <anderson.o.oliveira@hotmail.com>
2021-02-23 15:43:31 -0300

    Refatorando - Ordenação por Inserção

--- src/br/com/alura/algoritmos/TestaOrdenacao.java
+++ src/br/com/alura/algoritmos/TestaOrdenacao.java
@@ -25,21 +25,33 @@ public class TestaOrdenacao {
 
     private static void novoSort(Produto[] produtos , int quantidadeDeElementos) {
 
-        for (int atual = 0; atual < quantidadeDeElementos; atual++) {
+        for (int atual = 1; atual < quantidadeDeElementos; atual++) {
+
+            // System.out.println("Estou na casinha " + atual);
 
             int analise = atual;
 
             while (analise > 0 && produtos[analise].getPreco() < produtos[analise - 1].getPreco()){
-                Produto produtoAnalise = produtos[analise];
-                Produto produtoAnaliseMenos1 = produtos[analise - 1];
 
-                produtos[analise] = produtoAnaliseMenos1;
-                produtos[analise - 1] = produtoAnalise;
+                // System.out.println("Estou trocando " + analise + " com " + (analise -1));
+
+                troca(produtos, analise, analise -1);
+
                 analise--;
             }
-            
         }
-        
+    }
+
+    private static void troca(Produto[] produtos, int primeiro, int segundo) {
+
+        Produto primeiroProduto = produtos[primeiro];
+        Produto segundoProduto = produtos[segundo];
+
+        // System.out.println("Estou trocando " + primeiroProduto.getNome() + " com " + segundoProduto.getNome());
+
+        produtos[primeiro] = segundoProduto;
+        produtos[segundo] = primeiroProduto;
+
     }
 
     private static void selectionSort(Produto[] produtos , int quantidadeDeElementos) {
@@ -52,13 +64,8 @@ public class TestaOrdenacao {
 
             // System.out.println("Trocando " + atual + " com o " + menor);
 
-            Produto produtoAtual = produtos[atual]; //
-            Produto produtoMenor = produtos[menor]; //
+            troca(produtos, atual, menor);
 
-            // System.out.println("Trocando "+ produtoAtual.getNome() + " com o " + produtoMenor.getNome());
-
-            produtos[atual] = produtoMenor; // produto 2 brasilia
-            produtos[menor] = produtoAtual; // produto 0 lamborguini
         }
     }
 
