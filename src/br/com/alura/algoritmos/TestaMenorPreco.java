a560a0b5758f71e04ac49196c1d402ebdd8c673a
Anderson de Oliveira <anderson.o.oliveira@hotmail.com>
2021-02-23 09:03:33 -0300

    Refatorando - extraindo uma função

--- src/br/com/alura/algoritmos/TestaMenorPreco.java
+++ src/br/com/alura/algoritmos/TestaMenorPreco.java
@@ -12,13 +12,22 @@ public class TestaMenorPreco {
         produtos[4] = new Produto("Fusca" , 17000);
         */
 
-        Produto produto[] = {
+        Produto[] produtos = {
                 new Produto("Lamborghini" , 1000000),
                 new Produto("Jipe" , 46000),
                 new Produto("Brasília" , 16000),
                 new Produto("Smart" , 46000),
                 new Produto("Fusca" , 17000)
         };
+
+        int maisBarato = buscaMenor(produtos);
+
+
+        System.out.println(maisBarato);
+        System.out.println("O Carro " + produtos[maisBarato].getNome() + " é o mais barato e custa " + produtos[maisBarato].getPreco());
+    }
+
+    private static int buscaMenor(Produto[] produtos) {
 
         int maisBarato = 0;
 
@@ -28,7 +37,7 @@ public class TestaMenorPreco {
                 maisBarato = atual;
             }
         }
-        System.out.println(maisBarato);
-        System.out.println("O Carro " + produtos[maisBarato].getNome() + " é o mais barato e custa " + produtos[maisBarato].getPreco());
+        return maisBarato;
     }
+
 }
